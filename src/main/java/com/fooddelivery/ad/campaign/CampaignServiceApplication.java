64342891b739f64e1db4b14ef14196471de578a8
Parthu <parthudhanireddy@gmail.com>
2026-08-22 15:37:15 +0530

    Fix schema validation and configuration errors

--- src/main/java/com/fooddelivery/ad/campaign/CampaignServiceApplication.java
+++ src/main/java/com/fooddelivery/ad/campaign/CampaignServiceApplication.java
@@ -9,17 +9,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"com.fooddelivery.ad.campaign",
-    "com.fooddelivery.common", "com.fooddelivery"})
-@EntityScan(basePackages = {
-    "com.fooddelivery.ad.campaign.entity",
-    "com.fooddelivery.common.outbox.entity",
-    "com.fooddelivery.common.entity"
-})
-@EnableJpaRepositories(basePackages = {"com.fooddelivery"})
+@SpringBootApplication(
+    scanBasePackages = {"com.fooddelivery.ad.campaign", "com.fooddelivery.common"}
+)
+@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.ad.campaign", "com.fooddelivery.common.entity"})
+@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.ad.campaign", "com.fooddelivery.common.repository"})
 @EnableJpaAuditing
 @EnableScheduling
 @EnableFeignClients(basePackages = "com.fooddelivery.ad.campaign.client")
+@com.fooddelivery.common.outbox.config.EnableOutbox
 public class CampaignServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampaignServiceApplication.class, args);
